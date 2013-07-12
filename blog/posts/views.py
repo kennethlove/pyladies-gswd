b3f2552a8d446ea0013bb5752e2934bd5ce34bd9
Kenneth Love <kenneth@gigantuan.net>
2013-07-12 13:01:14 -0700

    views use new methods

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -6,13 +6,13 @@ from .models import Post
 
 def post_list_view(request):
     return render(request, 'posts/post_list.html', {
-        'post_list': Post.objects.live()
+        'post_list': Post.live.all()
     })
 
 
 def post_detail_view(request, slug):
     try:
-        post = Post.objects.live().get(slug=slug)
+        post = Post.live.get(slug=slug)
     except Post.DoesNotExist:
         raise Http404("That post doesn't exist")
 
