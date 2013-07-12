8be52ab293efa174941e4eb9a138d62dc2156040
Kenneth Love <kenneth@gigantuan.net>
2013-07-12 11:45:49 -0700

    fbv for list

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render
+
+from .models import Post
+
+
+def post_list_view(request):
+    return render(request, 'posts/post_list.html', {
+        'post_list': Post.objects.all()
+    })
+
