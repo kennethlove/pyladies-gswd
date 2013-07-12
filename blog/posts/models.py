2e1cc7b1fbdac19999bb0b0ad022847126264730
Kenneth Love <kenneth@gigantuan.net>
2013-07-12 10:54:46 -0700

    first post model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=2048)
+    content = models.TextField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(default=timezone.now)
+
