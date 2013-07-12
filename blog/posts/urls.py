839beb2ca8bba1fcd731e0d5f114a0ba3608ca18
Kenneth Love <kenneth@gigantuan.net>
2013-07-12 12:15:37 -0700

    detail view

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.post_list_view, name='list'),
+    url(r'^read/(?P<pk>\d+)/$', views.post_detail_view, name='detail'),
 )
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -8,3 +8,9 @@ def post_list_view(request):
         'post_list': Post.objects.all()
     })
 
+
+def post_detail_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {
+        'post': post
+    })
