ab790576573eb533ebf8df8eba2cfd02cf89e47e
Kenneth Love <kenneth@gigantuan.net>
2013-07-12 11:24:00 -0700

    stub templates and templateview

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.foo.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
