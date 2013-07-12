d44366316d569104654e126cac63b525fadde2a9
Kenneth Love <kenneth@gigantuan.net>
2013-07-12 12:42:05 -0700

    auto-set dates

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -6,9 +6,13 @@ class Post(models.Model):
     title = models.CharField(max_length=2048)
     slug = models.SlugField(max_length=2048)
     content = models.TextField()
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(blank=True, null=True, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now, editable=False)
 
     def __unicode__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.created_at = timezone.now()
+        super(Post, self).save(*args, **kwargs)
